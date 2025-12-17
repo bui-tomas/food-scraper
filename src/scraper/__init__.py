e75be1ce34d4e8254060855f99b27c3160807b88
bui-tomas <tomastuan.buianh@gmail.com>
2025-12-17 14:34:10 +0100

    deleted leftover files from src

--- src/scraper/__init__.py
+++
@@ -1,6 +0,0 @@
-from .cenyslovensko_scraper import FoodScraper
-from .database import Database
-from .config import CATEGORIES, TEST_CATEGORIES
-from .notifier import Notifier
-
-__all__ = ['FoodScraper', 'Database', 'Notifier', CATEGORIES, TEST_CATEGORIES]
